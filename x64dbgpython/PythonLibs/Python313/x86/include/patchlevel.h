c7446b2bcdf6e902e22f5035fcc7a9a29e6c0cd3
Mobile34 <43335665+mefkureci@users.noreply.github.com>
2026-02-18 21:27:48 +0300

    Create patchlevel.h for Python 3.13.9 definitions for x86 architecture

---
+++ x64dbgpython/PythonLibs/Python313/x86/include/patchlevel.h
@@ -0,0 +1,16 @@
+/*
+ * patchlevel.h
+ *
+ * Version: 3.13.9
+ */
+
+#define PY_MAJOR_VERSION 3
+#define PY_MINOR_VERSION 13
+#define PY_MICRO_VERSION 9
+#define PY_VERSION "3.13.9"
+#define PY_VERSION_HEX 0x030D09F0
+#define PY_RELEASE_LEVEL 0
+#define PY_RELEASE_SERIAL 0
+#define PY_LITTLE_ENDIAN 1
+#define PY_BIG_ENDIAN 0
+#include <limits.h>
